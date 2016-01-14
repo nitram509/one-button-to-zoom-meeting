ac6e0673935344026a287672683a884ff95ca7c2
nitram509 <maki@bitkings.de>
2016-01-14 17:02:26 +0100

    prefer to use /join API to make use of the generated ID (/start will always create a new meeting id)

--- start_meeting_and_notify_users.js
+++ start_meeting_and_notify_users.js
@@ -2,9 +2,19 @@
 
 //
 // ***** configure your settings here *****
+
+// loginTypes (copy from https://support.zoom.us/hc/en-us/articles/201363033-REST-User-API):
+// FACEBOOK = 0
+// GOOGLE = 1
+// API = 99
+// ZOOM = 100
+// SSO = 101
+var zoomUserLoginType = 100
+var zoomUserEmail = '<YOUR EMAIL HERE>'
 var zoomApiKey = '<YOUR API KEY HERE>'
 var zoomApiSecret = '<YOUR API SECRET HERE>'
 var meetingTopicPrefix = 'Instant Meeting'
+
 // *****************DONE*******************
 //
 
@@ -25,14 +35,28 @@ function doShellScriptAndParseResult (command) {
       'Result: \n' + responseStr)
 }
 
-function zoomGetUserList () {
+// *** not used at the moment
+// function zoomGetUserList () {
+//   var curlCommand = 'curl -XPOST '
+//   curlCommand += '--data "'
+//   curlCommand += 'api_key=' + encodeURI(zoomApiKey)
+//   curlCommand += '&api_secret=' + encodeURI(zoomApiSecret)
+//   curlCommand += '&data_type=JSON'
+//   curlCommand += '" '
+//   curlCommand += 'https://api.zoom.us/v1/user/list'
+//   return doShellScriptAndParseResult(curlCommand)
+// }
+
+function zoomGetUserByEmail (email, loginType) {
   var curlCommand = 'curl -XPOST '
   curlCommand += '--data "'
   curlCommand += 'api_key=' + encodeURI(zoomApiKey)
   curlCommand += '&api_secret=' + encodeURI(zoomApiSecret)
   curlCommand += '&data_type=JSON'
+  curlCommand += '&email=' + encodeURI(email)
+  curlCommand += '&login_type=' + encodeURI(loginType)
   curlCommand += '" '
-  curlCommand += 'https://api.zoom.us/v1/user/list'
+  curlCommand += 'https://api.zoom.us/v1/user/getbyemail'
   return doShellScriptAndParseResult(curlCommand)
 }
 
@@ -59,11 +83,22 @@ function killRunningZoomApp () {
   }
 }
 
-function startMeetingLocally (meetingId, userId, userName) {
-  // "zoommtg://zoom.us/start?confno=622266174&zc=0&uname=foobar&uid=SmX7qrWcQyu3QhjPS-D1jg"
-  var command = 'open  "zoommtg://zoom.us/start?'
-  command += 'confno=' + encodeURI(meetingId)
-  command += '&uid=' + encodeURI(userId)
+// *** not used at the moment
+// function startMeetingLocally (meetingId, userId, userName) {
+//   // "zoommtg://zoom.us/start?confno=622266174&zc=0&uname=foobar&uid=SmX7qrWcQyu3QhjPS-D1jg"
+//   var command = 'open  "zoommtg://zoom.us/start?'
+//   command += 'confno=' + encodeURI(meetingId)
+//   command += '&uid=' + encodeURI(userId)
+//   command += '&uname=' + encodeURI(userName)
+//   command += '&zc=0'
+//   command += '"'
+//   return doShellScript(command)
+// }
+
+function joinMeetingLocally (meetingId, userName) {
+  // "zoommtg://zoom.us/join?action=join&confno=119453959&zc=0&mcv=0.92.11227.0929&uname=Betty"
+  var command = 'open  "zoommtg://zoom.us/join?action=join'
+  command += '&confno=' + encodeURI(meetingId)
   command += '&uname=' + encodeURI(userName)
   command += '&zc=0'
   command += '"'
@@ -91,8 +126,7 @@ function sendSkypeMessage (skypeUser, message) {
   return doShellScript(command)
 }
 
-var userList = zoomGetUserList()
-var user = userList.users[0]
+var user = zoomGetUserByEmail(zoomUserEmail, zoomUserLoginType)
 var userName = user.first_name + ' ' + user.last_name
 var meetingTopic = meetingTopicPrefix + ' - ' + new Date().toDateString()
 var meeting = zoomCreateMeeting(user.id, meetingTopic)
@@ -106,7 +140,7 @@ console.log('### id        : ' + meeting.id)
 console.log('### user name : ' + userName)
 console.log('### topic     : ' + meetingTopic)
 console.log('### join URL  : ' + meeting.join_url)
-startMeetingLocally(meeting.id, user.id, userName)
+joinMeetingLocally(meeting.id, userName)
 console.log('# Meeting start initiated.')
 
 console.log('# Searching for Skype users to be notified ...')
